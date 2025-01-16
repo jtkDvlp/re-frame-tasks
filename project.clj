119e75f6accaa91958710b268069567fd9241002
jtkDvlp <jtkdevelopments@gmail.com>
2025-01-16 22:26:28 +0100

    Release 2.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jtk-dvlp/re-frame-tasks "2.0.1-SNAPSHOT"
+(defproject jtk-dvlp/re-frame-tasks "2.0.1"
   :description
   "A re-frame interceptor and helpers to register / unregister (background-)tasks"
 
